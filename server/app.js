e9aed91da4c9014099ad78f1001029ede2507d0d
shathakh <shathaqudaih17@gmail.com>
2022-08-24 14:28:48 +0300

    Sdsad

--- server/app.js
+++ server/app.js
@@ -1,15 +1,17 @@
-const express = require('express');
-
 const { join } = require('path');
+const express = require('express');
+require('dotenv').config();
 const router = require('./router');
 
 const app = express();
+
+app.set('port', process.env.PORT || 7000);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(join(__dirname, '..', 'public')));
 
 app.use('/api/v1', router);
 
-app.set('port', process.env.PORT || 7000);
 
 module.exports = app;
