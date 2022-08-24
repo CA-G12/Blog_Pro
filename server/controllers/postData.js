1f8e289a50fda0b74fdb8cb4c147998609f0fcc6
husamkamal <husamkmal94@gmail.com>
2022-08-24 12:54:23 +0300

    fix the test

--- server/controllers/postData.js
+++ server/controllers/postData.js
@@ -1,6 +1,7 @@
 const { post } = require('../database/queries');
 
 const postHandler = (req, res) => {
+  console.log('got a request')
   const { category, image, content } = req.body;
   post({ category, image, content }).then((data) => {
     res.json({
