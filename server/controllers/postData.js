5c76ac724caa85f137a7e102b189f39045320179
husamkamal <husamkmal94@gmail.com>
2022-08-24 13:01:28 +0300

    fix the port

--- server/controllers/postData.js
+++ server/controllers/postData.js
@@ -1,7 +1,6 @@
 const { post } = require('../database/queries');
 
 const postHandler = (req, res) => {
-  console.log('got a request')
   const { category, image, content } = req.body;
   post({ category, image, content }).then((data) => {
     res.json({
